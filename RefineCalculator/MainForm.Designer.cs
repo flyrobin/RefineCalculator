523b3a509704cc7db299a2f52e954ab393c73766
flyrobin <flyrobin620@hotmail.com>
2014-06-17 14:48:56 +0800

    Initial check in.

---
+++ RefineCalculator/MainForm.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace RefineCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label12, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBox6, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBox5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(139, 170);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label12.Location = new System.Drawing.Point(124, 147);
+            this.label12.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(12, 23);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "%";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox6.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox6.Location = new System.Drawing.Point(69, 143);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(49, 21);
+            this.textBox6.TabIndex = 26;
+            this.textBox6.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label11.Location = new System.Drawing.Point(3, 147);
+            this.label11.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 23);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "6级五行石";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label10.Location = new System.Drawing.Point(124, 119);
+            this.label10.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(12, 21);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "%";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox5.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox5.Location = new System.Drawing.Point(69, 115);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(49, 21);
+            this.textBox5.TabIndex = 23;
+            this.textBox5.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label9.Location = new System.Drawing.Point(3, 119);
+            this.label9.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 21);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "5级五行石";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Location = new System.Drawing.Point(124, 91);
+            this.label8.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(12, 21);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "%";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox4.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox4.Location = new System.Drawing.Point(69, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 21);
+            this.textBox4.TabIndex = 20;
+            this.textBox4.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Location = new System.Drawing.Point(3, 91);
+            this.label7.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 21);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "4级五行石";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(124, 63);
+            this.label6.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 21);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "%";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox3.Location = new System.Drawing.Point(69, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 21);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 63);
+            this.label5.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 21);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "3级五行石";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(124, 35);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 21);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "%";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox2.Location = new System.Drawing.Point(69, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 21);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 35);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 21);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "2级五行石";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(124, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "%";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox1.Location = new System.Drawing.Point(69, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 21);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1级五行石";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoEllipsis = true;
+            this.labelResult.Location = new System.Drawing.Point(157, 32);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(142, 116);
+            this.labelResult.TabIndex = 9;
+            this.labelResult.Text = "请在左侧输入单个五行石的成功几率";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "总在最前（关）";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 206);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "装备精炼计算器";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RefineCalculator/MainForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefineCalculator
+{
+    public partial class MainForm : Form
+    {
+        private const string DefaultMessage = "请在左侧输入单个五行石的成功几率";
+
+        private const int MaxSlots = 16;
+
+        private const int VeryLargeCost = 10000;
+
+        private const string TopMostOffString = "总在最前（关）";
+
+        private const string TopMostOnString = "总在最前（开）";
+
+        private static readonly string[] StoneLevels = new string[] { "一", "二", "三", "四", "五", "六" };
+
+        private static readonly int[] StoneCosts = new int[] { 1, 4, 13, 29, 146, 377 };
+
+        private TextBox[] inputBoxes = new TextBox[6];
+
+        private int?[,] costTable;
+
+        private int[,] pathTable = new int[10001, MaxSlots + 1];
+
+        private int?[] chances = new int?[6];
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.inputBoxes[0] = this.textBox1;
+            this.inputBoxes[1] = this.textBox2;
+            this.inputBoxes[2] = this.textBox3;
+            this.inputBoxes[3] = this.textBox4;
+            this.inputBoxes[4] = this.textBox5;
+            this.inputBoxes[5] = this.textBox6;
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            Array.Clear(this.chances, 0, this.chances.Length);
+            this.costTable = new int?[10001, MaxSlots + 1];
+
+            try
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(this.inputBoxes[i].Text))
+                    {
+                        this.chances[i] = (int)(Convert.ToDouble(this.inputBoxes[i].Text) * 100);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                labelResult.Text = DefaultMessage;
+                return;
+            }
+
+            int cost = this.Calculate(10000, MaxSlots);
+
+            if (cost >= VeryLargeCost)
+            {
+                labelResult.Text = "当前输入的等级的石头不足以达到100%成功率。请输入更高等级的石头。";
+            }
+            else
+            {
+                int[] numberOfStones = new int[6];
+                int remainChance = 10000;
+                for (int i = MaxSlots; i > 0 && remainChance > 0; i--)
+                {
+                    int usedStone = this.pathTable[remainChance, i];
+                    numberOfStones[usedStone]++;
+                    remainChance -= this.chances[usedStone].Value;
+                }
+
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < 6; i++)
+                {
+                    if (numberOfStones[i] > 0)
+                    {
+                        result.AppendFormat("{0}级五行石{1}颗\n", StoneLevels[i], numberOfStones[i]);
+                    }
+                }
+
+                labelResult.Text = result.ToString();
+            }
+        }
+
+        private int Calculate(int remainChance, int remainSlots)
+        {
+            if (remainChance <= 0)
+            {
+                return 0;
+            }
+
+            if (remainSlots <= 0)
+            {
+                return VeryLargeCost;
+            }
+
+            if (this.costTable[remainChance, remainSlots].HasValue)
+            {
+                return this.costTable[remainChance, remainSlots].Value;
+            }
+
+            int min = VeryLargeCost;
+            for (int i = 0; i < 6; i++)
+            {
+                if (this.chances[i].HasValue)
+                {
+                    int cost = this.Calculate(remainChance - this.chances[i].Value, remainSlots - 1) + StoneCosts[i];
+                    if (cost < min)
+                    {
+                        min = cost;
+                        this.pathTable[remainChance, remainSlots] = i;
+                    }
+                }
+            }
+
+            this.costTable[remainChance, remainSlots] = min;
+            return min;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+            button1.Text = this.TopMost ? TopMostOnString : TopMostOffString;
+        }
+    }
+}
